acd4a809f6aacd163607924c3361908495a3a06b
purlieu-studios <purliestudios.develop@gmail.com>
2025-09-08 23:52:41 -0500

    fix: switch from XUnit to NUnit testing framework

--- src/Purlieu.Ecs.Tests/BasicTests.cs
+++ src/Purlieu.Ecs.Tests/BasicTests.cs
@@ -1,28 +1,29 @@
-using Xunit;
+using NUnit.Framework;
 
 namespace Purlieu.Ecs.Tests;
 
+[TestFixture]
 public class BasicTests
 {
-    [Fact]
+    [Test]
     public void API_BasicSetup_ShouldPass()
     {
         // This is a placeholder test to ensure the test framework is working
-        Assert.True(true);
+        Assert.That(true, Is.True);
     }
 
-    [Fact]
+    [Test]
     public void ALLOC_NoAllocation_ShouldPass()
     {
         // Placeholder allocation test
         var result = 1 + 1;
-        Assert.Equal(2, result);
+        Assert.That(result, Is.EqualTo(2));
     }
 
-    [Fact]
+    [Test]
     public void IT_Integration_ShouldPass()
     {
         // Placeholder integration test
-        Assert.NotNull("test");
+        Assert.That("test", Is.Not.Null);
     }
 }
